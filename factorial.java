d16227446dcd0a5f411772b42220df5b3f1fba9f
lavanyakumar96 <lavanya.muthulakshmi@gmail.com>
2016-08-24 17:57:28 +0530

    Add files via upload

---
+++ factorial.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+class factorial
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+long fact=1;
+long n=sc.nextLong();
+for(int i=1;i<=n;i++)
+{
+fact=fact*i;
+}
+System.out.println(" the factorial is" +fact);
+}
+}
