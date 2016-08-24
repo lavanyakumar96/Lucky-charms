dfc3aeb64fb49a6cd99d90f71ee22c3f8f7c16bb
lavanyakumar96 <lavanya.muthulakshmi@gmail.com>
2016-08-24 18:00:21 +0530

    Update factorial.java

--- factorial.java
+++ factorial.java
@@ -7,9 +7,12 @@ public static void main(String args[])
 Scanner sc=new Scanner(System.in);
 long fact=1;
 long n=sc.nextLong();
+if(n>1)
+{
 for(int i=1;i<=n;i++)
 {
 fact=fact*i;
+}
 }
 System.out.println(" the factorial is" +fact);
 }
